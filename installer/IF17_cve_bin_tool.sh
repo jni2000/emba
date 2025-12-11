71165bf14d340aace465f5363db1af188eabb8a5
James Ni <11164076+jni2000@users.noreply.github.com>
2025-12-10 18:34:28 -0800

    Update IF17_cve_bin_tool.sh
    Disable cve database update

--- installer/IF17_cve_bin_tool.sh
+++ installer/IF17_cve_bin_tool.sh
@@ -86,7 +86,7 @@ IF17_cve_bin_tool() {
         fi
 
         echo "[*] Updating CVE database"
-        python3 ./cve_bin_tool/cli.py --update now -n json-mirror || true
+        # python3 ./cve_bin_tool/cli.py --update now -n json-mirror || true
         # testing db update
         python3 ./cve_bin_tool/cli.py -i ./cve_bin_tool_health_check.csv --disable-version-check --disable-validation-check --no-0-cve-report --offline -f csv -o /tmp/cve_bin_tool_health_check_results || true
 
